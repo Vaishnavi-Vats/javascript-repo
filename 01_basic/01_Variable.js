bddf3edb8bbc7b1374614657c8a5524d0168890f
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-23 18:22:59 +0000

    variable file created

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId=32356;
+ let accountEmail="vaishxyz@gmail.com";
+ var accountPasword="324533";
+ accountCity="Delhi"
+ //accountId=5869433;  it throws error because of constant assignment
+ //console.log(accountId);
+ accountEmail=" tanishq23@gmail.com";
+ accountPasword="56677";
+ accountCity="Noida";
+ console.table([accountId,accountPasword,accountEmail,accountCity]);
+/* prefer not to use variable var because of issue in block scope and functional scope*/
