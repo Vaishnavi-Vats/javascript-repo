d7696c23974cebede2ac767d1555dc82d5a99dae
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-24 18:10:04 +0000

    study of data types

---
+++ 01_basic/Data1.js
@@ -0,0 +1,18 @@
+let name = "Vaishanvi ";
+let age= 21;
+let isLoggedIn= false;
+
+
+//number=> range
+//BigInt=>String
+//boolean=> yes or no
+//null=> stand alone value
+//undefined
+//symbol=> finnd uniqueness used
+// object
+
+
+
+
+console.log(typeof null);
+console.log(typeof undefined);
