3b648ec4546afc96e2277d32aabbb70470694dbd
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-24 18:53:48 +0000

    Study of conversion of data type

---
+++ 01_basic/03_datatypeconv.js
@@ -0,0 +1,25 @@
+let abc ="vaishnavi";
+console.log(typeof abc);
+
+let valueInNumber= Number (abc);
+console.log(typeof valueInNumber);
+
+//"33" can easily conver into number 
+// "3aba" this will gives us the value NaN
+// true will be converted into 1
+
+
+let isLoggedIn="";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//1= true
+//0 = false
+//" "= false
+//"abc"= true
+
+let sumNumber=33;
+let stringNumber=String(sumNumber);
+console.log(sumNumber);
+console.log(typeof sumNumber);
