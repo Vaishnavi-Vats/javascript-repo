26fa472604136e7ae0b0644967c70705c632bd31
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-25 17:43:37 +0000

    learning of basic operations

--- 01_basic/Data1.js
+++ 01_basic/Data1.js
@@ -12,7 +12,27 @@ let isLoggedIn= false;
 // object
 
 
-
-
 console.log(typeof null);
 console.log(typeof undefined);
+
+// operation
+let value = 3;
+let NegValue = - value; 
+// console.log(NegValue);
+
+let str = "RADHE";
+let str2 ="Shyam";
+let str3 = str + str2;
+// console.log(NegValue);
+// console.log(1+"2");
+// console.log("1"+1+"2");
+// console.log("1"+2);
+// console.log(2+1+"2");
+// console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3;
+num1=num2=num3=2+2;
+let gameCounter =100;
+gameCounter++;
+ console.log(gameCounter);
