a09c1d99fe85dd72ba1317ff3121b91d30df97f4
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-26 18:01:16 +0000

    added chnges

---
+++ 01_basic/04_datatypesumm.js
@@ -0,0 +1,14 @@
+// Primitive
+//String ---- rep seq of char
+//number. --- rep all numbers
+//notdefined
+//bigInt
+//etc
+
+// refrence type
+// object
+// array 
+// etc
+
+const value = 92929131313144;
+console.log(typeof value);
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,5 +1,1 @@
-
- let a=10;
- let b=23;
- let c=a+b;
- console.log(c);
+console.log("heloo")
