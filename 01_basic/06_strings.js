bb5bdea7f932a1622c6c2a278ce93eecf58c2583
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-27 18:18:54 +0000

    strings file added

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,18 @@
+const name = "Tanishq"
+const repocount = 3;
+// tanishq 3 value ---- output
+//console.log(`the name is ${name} and the repocount is ${repocount} with some value`);
+
+// always prefer backticks when you need to use two or three variables . 
+
+//--------------------------------------------------------------------------
+
+const gameName = new String ("tan-ishq")
+// console.log(gameName[0].toUpperCase());
+// console.log(gameName.indexOf("n"));
+// console.log(gameName.charAt(2));
+
+ console.log(gameName.substring(0,3));          // substring
+
+//-----------------------------------------------------------------------------
+
