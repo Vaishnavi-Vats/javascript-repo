aca05c4e54e89b2313e093fa12feca253bf95ea2
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-28 18:12:16 +0000

    maths and number file saved

---
+++ 01_basic/07_maths_number.js
@@ -0,0 +1,40 @@
+// number. - -- datatype
+const score = 300;
+//console.log(score);
+// syntax for specify the datatype
+const profit = new Number(200);
+//console.log(profit);
+
+//-------Some specific functions ----------------//
+
+/// to string
+
+// console.log(profit.toString().length);  // it converts number into string
+// console.log(typeof profit);
+
+// to fixed
+const ammount = 100;
+// console.log(ammount.toFixed(2));    // it gives us the pricison value
+
+// to precesion 
+const anotherAmmount = 123.8978;
+// console.log(anotherAmmount.toPrecision(3)); // gives us the precision value 
+
+// toLocaleSting
+const loss = 10000000;
+// console.log(loss.toLocaleString('en-IN'));  // this give us standerd value 
+
+// ++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++//
+// console.log(Math.abs(-6));  // it converts negeative value into positive value
+// console.log(Math.round(4.6)); // it give the round of value 
+
+// console.log(Math.ceil(4.5));  // it give the max value
+// console.log(Math.floor(3.9)); // it give the min value
+
+// console.log((Math.random()*10)+1); // it give the random number btw 1 to infinite 
+
+const min = 10;
+const max = 20;
+console.log((Math.random));
+
+
