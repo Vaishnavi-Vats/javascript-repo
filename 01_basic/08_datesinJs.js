98fc33e2a8a2543921177051e271afbb97f8cb48
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-09-29 17:52:43 +0000

    date time file added

---
+++ 01_basic/08_datesinJs.js
@@ -0,0 +1,10 @@
+// date in java script ----------
+
+let currentDate = new Date();
+// console.log(currentDate.toString());  // give standered date and time
+// console.log(currentDate.toLocaleString('en-IN')); // GIVES THE DATE IN INDIAN STANDERED TIME 
+// console.log(currentDate.toDateString()); //give little readable standerd
+// console.log(currentDate.toISOString()); //give the iso format
+
+let createdDate = new Date("2023-09-23");
+// console.log(createdDate.toLocaleString());
