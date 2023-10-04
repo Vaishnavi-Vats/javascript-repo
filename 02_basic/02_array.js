54e225f3ff1885c32f6b0c9337e4860b72ad2bc6
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-10-04 16:58:55 +0000

    practice

---
+++ 02_basic/02_array.js
@@ -0,0 +1,16 @@
+  const friend = ['Tanishq','Vaishnavi','Rabiya','Riya'];
+  const section = ['3B','3A','4A','4B'];
+
+  // if I will push sectoion array into friends array the o/p will be
+  // friends.push(section);
+  // console.log(friend); 
+  // it will show that the section is added as a single component in the friend
+
+  const result=friend.concat(section);
+  //console.log(result);   // concat returns new array
+  const newres=[...friend,...section];
+  //console.log(newres);  // this is the spread operator method
+ 
+  const Arr1 = [1,2,3,4,5,6,7,8,[23,56,[67]]];
+  console.log(Arr1.flat(Infinity));
+
