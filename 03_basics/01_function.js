e744d527eb710d1a021e382118dfc1587700ca5e
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-10-05 05:35:40 +0000

    practice

---
+++ 03_basics/01_function.js
@@ -0,0 +1,31 @@
+// function myName(){
+//     console.log("V");
+//     console.log("A");
+//     console.log("I");
+//     console.log("S");
+//     console.log("H");
+//     console.log("N");
+//     console.log("A");
+//     console.log("V");
+//     console.log("I");
+
+// }
+ 
+//  let a=myName();
+//  console.log(a);
+
+function add( a, b){
+return a+b;
+}
+add(34,56);
+let c =add(34,67);
+console.log(c);
+
+function userLoginMessage(username){
+    return `This ${username} has logged in`
+}
+let a= userLoginMessage("Vaishnavi");
+console.log(userLoginMessage);
+
+
+
