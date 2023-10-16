a1af7cb00b05475827020f8e4200a088aad38b3c
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-10-16 17:38:18 +0000

    practice

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -1,13 +1,13 @@
 // In this we talk about local and global scope 
 
 // var c = 300 ;    // in var case this will show diff value
-let a = 300 ;     // global scope
+let an = 300 ;     // global scope
 if(true){
-    let a = 10;
+    let an = 10;
     const b = 20;
    //  console.log("Inner :" , a);   // block scope
 } 
-// console.log(a);    // show error
+// console.log(an);    // show error
 // console.log(b);    // show error
 // console.log(c);    // do not show any kind of error 
 
@@ -29,3 +29,37 @@ function one (){
 
 }
 // this  is the json format  of coading and it is called 
+
+
+
+
+
+
+
+let a=45;
+let b =89;
+let c=30;
+
+for(let i=0;i<10;i++){
+    console.log(i)
+    console.log(a+b);
+  
+    var v=56;
+    let c=3656;
+}
+
+console.log(c);
+
+
+function difference( a,b){
+    let diff = b-a;
+    return diff;
+    
+}
+   let result = difference(89,78)
+ console.log(result);
+ // scope of variable
+ // local scope= that can be access locally 
+ // global scope = that can be access anywhere entire the code
+ // fuction scope = variable that can be acess entire the function 
+ // lexical scope=A variable defined outside a funxtion can be accesibe inside another function defined agter variable declaration
